1635f673f4effa1c1e04731e4d25e0991d60f372
rilsaky-ux <rilsaky@gmail.com>
2026-03-28 14:24:53 +0000

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a17x.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a17x-user \
+    omni_a17x-userdebug \
+    omni_a17x-eng
---
+++ omni_a17x.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a17x device
+$(call inherit-product, device/samsung/a17x/device.mk)
+
+PRODUCT_DEVICE := a17x
+PRODUCT_NAME := omni_a17x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A176B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a17xxx-user 13 TP1A.220624.014 A176BXXU2BYID release-keys"
+
+BUILD_FINGERPRINT := samsung/a17xxx/a17x:13/TP1A.220624.014/A176BXXU2BYID:user/release-keys
